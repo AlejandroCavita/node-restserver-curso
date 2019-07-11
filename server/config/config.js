43a191ed018483fff72e32ea9da848dbc47162be
Alejandro Cavita <alejandrocavita@gmail.com>
2019-07-11 10:17:37 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,21 @@
 //========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//========================
+//Entorno
+//========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+
+//========================
+//Base de datos
+//========================
+
+let urlDB = (process.env.NODE_ENV === 'dev') ? 'mongodb://localhost:27017/cafe' : 'mongodb+srv://alejandrocavita:OnePiece92@cluster0-s99xg.mongodb.net/cafe';
+
+// urlDB = 'mongodb+srv://alejandrocavita:OnePiece92@cluster0-s99xg.mongodb.net/cafe';
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config')
 
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -11,40 +14,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/users'))
 
-app.get('/usuario', function(req, res) {
-    res.json('getUsuario')
-})
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
+    if (err) throw err;
 
-app.post('/usuario', function(req, res) {
+    console.log('Base de datos ONLINE');
+});
 
-    let body = req.body;
-    console.log(body.nombre);
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-})
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id
-
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('deleteUsuario')
-})
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando el puerto ', 3000);
